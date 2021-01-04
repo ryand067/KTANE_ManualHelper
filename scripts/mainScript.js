5097674f1213ceef7705fb55f8bcc6c2eefe1bf4
Ryand067 <RKD17@pitt.edu>
2021-01-03 23:29:17 -0500

    Started working on CrazyTalk Modifications

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,8 +1,36 @@
+//Class
+class CrazyTalkItem {
+    constructor(text, up, down) {
+        this.text = text;
+        this.up = up;
+        this.down = down;
+    }
+    
+    compare(items) {
+        return (this.text.localeCompare(items.text));
+    }
+}
+
 //Variable Array to hold all of the words needed for the 1,000 Words Module Helper
 var wordsFor5Words = [];
 var passes = ["about", "after", "again", "below", "could", "every", "first", "found", "great", "house", "large", "learn", "never", "other", "place", "plant", "point", "right", "small", "sound", "spell",
              "still", "study", "their", "there", "these", "thing", "think", "three", "water", "where", "which", "world", "would", "write"];
 var divs = ["addition", "chickenNuggets", "crazyTalk", "thousandWords", "modulo", "divisibleNumbers", "jackolantern", "tableA", "tableB", "tableC"];
+var CrazyTalkWords = [new CrazyTalkItem("< < > < > >", 5, 4), new CrazyTalkItem("1 3 2 4", 3, 2), new CrazyTalkItem("LEFT ARROW LEFT WORD RIGHT ARROW LEFT WORD RIGHT ARROW RIGHT WORD", 5, 8),
+                     new CrazyTalkItem("BLANK", 1, 3), new CrazyTalkItem("LITERALLY BLANK", 1, 5), new CrazyTalkItem("FOR THE LOVE OF ALL THAT IS GOOD AND HOLY PLEASE FULLSTOP FULLSTOP.", 9, 0),
+                     new CrazyTalkItem("AN ACTUAL LEFT ARROW LITERAL PHRASE", 5, 3), new CrazyTalkItem("FOR THE LOVE OF - THE DISPLAY JUST CHANGED, I DIDN'T KNOW THIS MOD COULD DO THAT. DOES IT MENTION THAT IN THE MANUAL?", 8, 7), new CrazyTalkItem("ALL WORDS ONE THREE TO FOR FOR AS IN THIS IS FOR YOU", 4, 0), new CrazyTalkItem("LITERALLY NOTHING", 1, 4), new CrazyTalkItem("NO, LITERALLY NOTHING", 2, 5),
+                     new CrazyTalkItem("THE WORD LEFT", 7, 0), new CrazyTalkItem("HOLD ON IT'S BLANK", 1, 9), new CrazyTalkItem("SEVEN WORDS FIVE WORDS THREE WORDS THE PUNCTUATION FULLSTOP", 0, 5), new CrazyTalkItem("THE PHRASE THE WORD STOP TWICE", 9, 1), new CrazyTalkItem("THE FOLLOWING SENTENCE THE WORD NOTHING", 2, 7), new CrazyTalkItem("ONE THREE TO FOR", 3, 9), new CrazyTalkItem("THREE WORDS THE WORD STOP", 7, 3), new CrazyTalkItem("DISREGARD WHAT I JUST SAID. FOUR WORDS, NO PUNCTUATION. ONE THREE 2 4.", 3, 1), new CrazyTalkItem("1 3 2 FOR", 1, 0), new CrazyTalkItem("DISREGARD WHAT I JUST SAID. TWO WORDS THEN TWO DIGITS. ONE THREE 2 4.", 0, 8), new CrazyTalkItem("WE JUST BLEW UP", 4, 2), new CrazyTalkItem("NO REALLY.", 5, 2), new CrazyTalkItem("< LEFT > LEFT > RIGHT", 5, 6),
+                     new CrazyTalkItem("ONE AND THEN 3 TO 4", 4, 7), new CrazyTalkItem("STOP TWICE", 7, 6), new CrazyTalkItem("LEFT", 6, 9), new CrazyTalkItem("..", 8, 5), new CrazyTalkItem("PERIOD PERIOD", 8, 2),
+                     new CrazyTalkItem("THERE ARE THREE WORDS NO PUNCTUATION READY? STOP DOT PERIOD", 5, 0), new CrazyTalkItem("NOVEBMER OSCAR SPACE, LIMA INDIGO TANGO ECHO ROMEO ALPHA LIMA LIMA YANKEE SPACE NOVEMBER OSCAR TANGO HOTEL INDEGO NOVEMBER GOLF", 2, 9), new CrazyTalkItem("FIVE WORDS THREE WORDS THE PUNCTUATION FULLSTOP", 1, 9), new CrazyTalkItem("THE PHRASE: THE PUNCTUATION FULLSTOP", 9, 3),
+                     new CrazyTalkItem("EMPTY SPACE", 1, 6), new CrazyTalkItem("ONE THREE TWO FOUR", 3, 7), new CrazyTalkItem("IT'S SHOWING NOTHING", 2, 3), new CrazyTalkItem("LIMA ECHO FOXTROT TANGO SPACE ALPHA ROMEO ROMEO OSCAR RISKY SPACE SIERRA YANKEE MIKE BRAVO OSCAR LIMA", 1, 2), new CrazyTalkItem("ONE 3 2 4", 3, 4), new CrazyTalkItem("STOP.", 7, 4), new CrazyTalkItem(".PERIOD", 8, 1),
+                     new CrazyTalkItem("NO REALLY STOP", 5, 1), new CrazyTalkItem("1 3 TOO 4", 2, 0), new CrazyTalkItem("PERIOD TWICE", 8, 3), new CrazyTalkItem("1 3 TOO WITH 2 OHS FOUR", 4,  2),
+                     new CrazyTalkItem("1 3 TO 4", 3, 0), new CrazyTalkItem("STOP DOT PERIOD", 5, 0), new CrazyTalkItem("LEFT LEFT RIGHT LEFT RIGHT RIGHT", 6, 7), new CrazyTalkItem("IT LITERALLY SAYS THE WORD ONE AND THEN THE NUMBERS 2 3 4", 4, 5), new CrazyTalkItem("ONE IN LETTERS 3 2 4 IN NUMBERS", 3, 5), new CrazyTalkItem("WAIT FORGET EVERYTHING I JUST SAID, TWO WORDS THEN TWO SYMBOLS THEN TWO WORDS: < < RIGHT LEFT > >", 1, 6), new CrazyTalkItem("1 THREE TWO FOUR", 3, 6), new CrazyTalkItem("PERIOD", 7, 9), new CrazyTalkItem(".STOP", 7, 8), new CrazyTalkItem("NOVEBMER OSCAR SPACE, LIMA INDIA TANGO ECHO ROMEO ALPHA LIMA LIMA YANKEE SPACE NOVEMBER OSCAR TANGO HOTEL INDIA NOVEMBER GOLF", 0, 7), new CrazyTalkItem("LIMA ECHO FOXTROT TANGO SPACE ALPHA ROMEO ROMEO OSCAR WHISKEY SPACE SIERRA YANKEE MIKE BRAVO OSCAR LIMA", 6, 5), new CrazyTalkItem("NOTHING", 1, 2), new CrazyTalkItem("THERE'S NOTHING", 1, 8), new CrazyTalkItem("STOP STOP", 7, 5), new CrazyTalkItem("RIGHT ALL IN WORDS STARTING NOW ONE TWO THREE FOUR", 4, 9), new CrazyTalkItem("THE PHRASE THE WORD LEFT", 7, 1), new CrazyTalkItem("LEFT ARROW SYMBOL TWICE THEN THE WORDS RIGHT LEFT RIGHT THEN A RIGHT ARROW SYMBOL", 5, 9), new CrazyTalkItem("LEFT LEFT RIGHT < RIGHT >", 5, 7), new CrazyTalkItem("NO COMMA LITERALLY NOTHING", 2, 4), new CrazyTalkItem("HOLD ON CRAZY TALK WHILE I DO THIS NEEDY", 2, 1),
+                     new CrazyTalkItem("THIS ONE IS ALL ARROW SYMBOLS NO WORDS", 2, 8), new CrazyTalkItem("<", 6, 3), new CrazyTalkItem("THE WORD STOP TWICE", 9, 4), new CrazyTalkItem("< < RIGHT LEFT > >", 6, 1), new CrazyTalkItem("THE PUNCTUATION FULLSTOP", 9, 2), new CrazyTalkItem("1 3 TOO WITH TWO OS 4", 4, 1), new CrazyTalkItem("THREE WORDS THE PUNCTUATION FULLSTOP", 9, 9),
+                     new CrazyTalkItem("OK WORD FOR WORD LEFT ARROW SYMBOL TWICE THEN THE WORDS RIGHT LEFT RIGHT THEN A RIGHT ARROW SYMBOL", 6, 0), new CrazyTalkItem("DOT DOT", 8, 6),
+                     new CrazyTalkItem("LEFT ARROW", 6, 8), new CrazyTalkItem("AFTER I SAY BEEP FIND THIS PHRASE WORD FOR WORD BEEP AN ACTUAL LEFT ARROW", 7, 2), new CrazyTalkItem("ONE THREE 2 WITH TWO OHS 4", 4, 3),
+                     new CrazyTalkItem("LEFT ARROW SYMBOL", 6, 4), new CrazyTalkItem("AN ACTUAL LEFT ARROW", 6, 2), new CrazyTalkItem("THAT'S WHAT IT'S SHOWING", 2, 1), new CrazyTalkItem("THE PHRASE THE WORD NOTHING", 2, 6), new CrazyTalkItem("THE WORD ONE AND THEN THE NUMBERS 3 2 4", 4, 8), new CrazyTalkItem("ONE 3 2 FOUR", 3, 8), new CrazyTalkItem("ONE WORD THEN PUNCTUATION. STOP STOP.", 0, 9),
+                     new CrazyTalkItem("THE WORD BLANK", 0, 1), new CrazyTalkItem("FULLSTOP FULLSTOP", 8, 4)];
+//var CrazyTalkWords = [new CrazyTalkItem("Hello", 1, 2), new CrazyTalkItem("World", 3, 4), new CrazyTalkItem("AzzyLand", 6, 8), new CrazyTalkItem('< < > < > >', 5, 4), new CrazyTalkItem('< LEFT > LEFT > RIGHT', 5, 6)];
 
 //Function to increase and decrease number of each color wires in Sequences
 function modifyCounters(color, direction) {
@@ -130,6 +158,23 @@ function additionGo() {
 	document.getElementById("additionTextArea").focus();
 }
 
+//Crazy Talk Function
+function CrazyTalkGo() {
+    
+    //Set input and outputs
+    var input = document.getElementById('CrazyTalkTextArea').value.toUpperCase();
+    var output = document.getElementById('CrazyTalkResponse');
+    document.getElementById('CrazyTalkTextArea').value = "";
+    
+    //If the search finds it, report to the user that it was found and the up and down values. Otherwise report that it wasn't
+    var bsRes = search(CrazyTalkWords, input);
+    if (bsRes === -1) {
+        output.innerHTML = "CrazyTalk <i>" + input + "</i> not found!";
+    } else {
+        output.innerHTML = "CrazyTalk <i>" + input + "</i> Found!<br> &emsp; UP: " + CrazyTalkWords[bsRes].up + "<br> &emsp; DOWN: " + CrazyTalkWords[bsRes].down;
+    }
+}
+
 //1,000 Words Function
 function findWord() {
     
@@ -168,6 +213,16 @@ function binarySearch(arr, word) {
         }
         if (res === 0) {
             return mid;
+        }
+    }
+    return -1;
+}
+
+//Search function for CrazyTalk
+function search(arr, phrase) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].text.localeCompare(phrase) === 0) {
+            return i;
         }
     }
     return -1;
@@ -235,6 +290,7 @@ function clr_fields() {
 	document.getElementById('blackbox').value = "";
 	document.getElementById('blackbox').value = 0;
 	document.getElementById('MemoryTextArea').value = "";
+    document.getElementById("CrazyTalkTextArea").value = "";
     document.getElementById('chickNugs').value = "";
     document.getElementById('chickNugsRes').innerHTML = "";
     document.getElementById('additionTextArea').value = "";
@@ -249,6 +305,7 @@ function clr_fields() {
     document.getElementById('modRes').innerHTML = "";
     document.getElementById('divisibleNum').value = "";
     document.getElementById('divisibleRes').innerHTML = "";
+    document.getElementById("CrazyTalkResponse").innerHTML = "";
     
     //Hide All Other Mods
     for (var a = 0; a < divs.length; a++) {
@@ -286,7 +343,7 @@ function splitLines(text) {
 function printArray(arrayObj) {
     var i = 0;
     for (i = 0; i < arrayObj.length; i++) {
-        console.log(i + ". " + arrayObj[i]);
+        //console.log(i + ". " + arrayObj[i]);
     }
 }
 
@@ -345,6 +402,10 @@ function loadData() {
 	readTextFile('./words.txt');
     autocomplete(document.getElementById("word5"), wordsFor5Words);
     autocomplete(document.getElementById("wordPass"), passes);
+    
+    for (var r = 0; r < CrazyTalkWords.length; r++) {
+        //console.log(CrazyTalkWords[r].text);
+    }
 }
 
 //Autocomplete Functions
