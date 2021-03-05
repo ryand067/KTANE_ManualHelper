d4290e178c6c375606743de240d164efa7a8d074
ryand067 <rkd17@pitt.edu>
2021-03-04 22:55:43 -0500

    Almost finished the Clue Mod

--- scripts/ClueTable.js
+++ scripts/ClueTable.js
@@ -11,7 +11,7 @@ function toggleCellColor(cellId) {
 
 function toggleCellNumColor(cellId) {
     var item = document.getElementById(cellId);
-    console.log("B Color: " + item.style.backgroundColor);
+    //console.log("B Color: " + item.style.backgroundColor);
     if (item.style.backgroundColor === "") {
         item.style.backgroundColor = "yellow";
     } else if (item.style.backgroundColor.localeCompare("yellow")) {
@@ -21,16 +21,30 @@ function toggleCellNumColor(cellId) {
     }
 }
 
-function colorToHex(color) {
-    if (color.substr(0, 1) === '#') {
-        return color;
+function loadFrm() {
+    var x = document.getElementById('tableD1');
+    var arrow = document.getElementById(divId.concat('Arrow'));
+
+    x.style.display = "none";
+    arrow.src = './images/rightArrow.png';
+}
+
+function clearCluePage() {
+    var tblElems = ["locTableScarlett", "locTablePlum", "locTablePeacock", "locTableGreen", "locTableMustard", "locTableWhite",
+    "locTableCandle", "locTableDagger", "locTablePipe", "locTableRevolver", "locTableRope", "locTableSpanner",
+    "locTableR1", "locTable1A", "locTable1B", "locTable1C", "locTable1D", "locTable1E", "locTable1F",
+    "locTableR2", "locTable2A", "locTable2B", "locTable2C", "locTable2D", "locTable2E", "locTable2F",
+    "locTableR3", "locTable3A", "locTable3B", "locTable3C", "locTable3D", "locTable3E", "locTable3F",
+    "locTableR4", "locTable4A", "locTable4B", "locTable4C", "locTable4D", "locTable4E", "locTable4F",
+    "locTableR5", "locTable5A", "locTable5B", "locTable5C", "locTable5D", "locTable5E", "locTable5F",
+    "locTableR6", "locTable6A", "locTable6B", "locTable6C", "locTable6D", "locTable6E", "locTable6F",
+    "locTableR7", "locTable7A", "locTable7B", "locTable7C", "locTable7D", "locTable7E", "locTable7F",
+    "locTableR8", "locTable8A", "locTable8B", "locTable8C", "locTable8D", "locTable8E", "locTable8F",
+    "locTableR9", "locTable9A", "locTable9B", "locTable9C", "locTable9D", "locTable9E", "locTable9F",]
+
+    for (var a = 0; a < tblElems.length; a++) {
+        document.getElementById(tblElems[a]).style.backgroundColor = "";
     }
-    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
 
-    var red = parseInt(digits[2]);
-    var green = parseInt(digits[3]);
-    var blue = parseInt(digits[4]);
-
-    var rgb = blue | (green << 8) | (red << 16);
-    return digits[1] + '#' + rgb.toString(16).padStart(6, '0');
-};
+    document.getElementById('tableD1').style.display = "none";
+}
